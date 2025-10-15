457d85c5ca66dc0f0321aa0c70a4988d096c22a3
Gascuena Leo <gascuenaleo@gmail.com>
2025-10-15 10:49:51 +0200

    ADD/Quiz page and

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Bonjour !", Toast.LENGTH_LONG).show();
+
+
+        Button Quizbutton = findViewById(R.id.Quizbutton);
+
+        Quizbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
